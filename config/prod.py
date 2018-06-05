2d23d4fab99751aa7eadb9211352c2651ab554ab
Bharat Ruparel <bharat@Bharats-MacBook-Pro.local>
2018-06-05 18:08:02 -0500

    getting ready for production deployment on Heroku

--- config/prod.py
+++ config/prod.py
@@ -1,4 +1,6 @@
+import os
+
 DEBUG = False
 SECRET_KEY = 'topsecret'
-SQLALCHEMY_DATABASE_URI = "postgresql://postgres:''@localhost/catalog_db"
+SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URI']
 SQLALCHEMY_TRACK_MODIFICATIONS = False
--- run.py
+++ run.py
@@ -1,11 +1,25 @@
+# from app import create_app, db
+# from app.auth.models import User
+
+# if __name__ == '__main__':
+#     flask_app = create_app('dev')
+#     with flask_app.app_context():
+#         db.create_all()
+#         if not User.query.filter_by(user_name='harry').first():
+#             User.create_user(user='harry', email='harry@potters.com', password='secret')
+
+#     flask_app.run()
+
+# The above commented out block is for development, the following is for production
 from app import create_app, db
 from app.auth.models import User
+from sqlalchemy import exc
 
-if __name__ == '__main__':
-    flask_app = create_app('prod')
-    with flask_app.app_context():
-        db.create_all()
+flask_app = create_app('prod')
+with flask_app.app_context():
+    db.create_all()
+    try:
         if not User.query.filter_by(user_name='harry').first():
             User.create_user(user='harry', email='harry@potters.com', password='secret')
-
-    flask_app.run()
+    except exc.IntegrityError:
+            flask_app.run()
